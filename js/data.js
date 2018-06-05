733f8a0bc413bae929d0383f96fe73b3df4004ae
cathey191 <cathey191@hotmail.com>
2018-06-06 10:45:12 +1200

    chart setup

---
+++ js/data.js
@@ -0,0 +1,26 @@
+var data = {
+	motorbike: {
+		price: 109,
+		minDay: 1,
+		maxDay: 5,
+		fuelKm: 3.7
+	},
+	smallCar: {
+		price: 129,
+		minDay: 1,
+		maxDay: 10,
+		fuelKm: 8.5
+	},
+	largeCar: {
+		price: 144,
+		minDay: 3,
+		maxDay: 10,
+		fuelKm: 9.7
+	},
+	motorhome: {
+		price: 200,
+		minDay: 2,
+		maxDay: 15,
+		fuelKm: 17
+	}
+}
