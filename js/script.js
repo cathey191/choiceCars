aacd30272ecb86d2469c1dcb22c2a0ef0325f890
cathey191 <cathey191@hotmail.com>
2018-06-06 11:39:38 +1200

    fixed array

--- js/script.js
+++ js/script.js
@@ -1,38 +1,34 @@
 var data = [
-	{ motorbike: {
-			mixSeat: 1,
-			maxSeat: 1,
-			price: 109,
-			minDay: 1,
-			maxDay: 5,
-			fuelKm: 3.7
-		}
+	motorbike = {
+		mixSeat: 1,
+		maxSeat: 1,
+		price: 109,
+		minDay: 1,
+		maxDay: 5,
+		fuelKm: 3.7
 	},
-	{ smallCar: {
-			mixSeat: 1,
-			maxSeat: 2,
-			price: 129,
-			minDay: 1,
-			maxDay: 10,
-			fuelKm: 8.5
-		}
+	smallCar = {
+		mixSeat: 1,
+		maxSeat: 2,
+		price: 129,
+		minDay: 1,
+		maxDay: 10,
+		fuelKm: 8.5
 	},
-	{ largeCar: {
-			mixSeat: 1,
-			maxSeat: 5,
-			price: 144,
-			minDay: 3,
-			maxDay: 10,
-			fuelKm: 9.7
-		}
+	largeCar = {
+		mixSeat: 1,
+		maxSeat: 5,
+		price: 144,
+		minDay: 3,
+		maxDay: 10,
+		fuelKm: 9.7
 	},
-	{ motorhome: {
-			mixSeat: 2,
-			maxSeat: 6,
-			price: 200,
-			minDay: 2,
-			maxDay: 15,
-			fuelKm: 17
-		}
+	motorhome = {
+		mixSeat: 2,
+		maxSeat: 6,
+		price: 200,
+		minDay: 2,
+		maxDay: 15,
+		fuelKm: 17
 	}
 ]
