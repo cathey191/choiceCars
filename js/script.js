48165e161d598534aa1a4de6ac6fca62bc73c236
cathey191 <cathey191@hotmail.com>
2018-06-20 09:30:28 +1200

    custom styling to chart

--- js/script.js
+++ js/script.js
@@ -290,32 +290,34 @@ $(document).ready(function() {
 						{
 							label: 'Total Price in NZD',
 							data: price,
-							backgroundColor: [
-								'rgba(255, 99, 132, 0.2)',
-								'rgba(54, 162, 235, 0.2)',
-								'rgba(255, 206, 86, 0.2)',
-								'rgba(75, 192, 192, 0.2)',
-								'rgba(153, 102, 255, 0.2)',
-								'rgba(255, 159, 64, 0.2)'
-							],
-							borderColor: [
-								'rgba(255,99,132,1)',
-								'rgba(54, 162, 235, 1)',
-								'rgba(255, 206, 86, 1)',
-								'rgba(75, 192, 192, 1)',
-								'rgba(153, 102, 255, 1)',
-								'rgba(255, 159, 64, 1)'
-							],
+							backgroundColor: ['#56a54d', '#3d733f', '#a6bbb8'],
+							borderColor: ['#3d733f', '#2f3c3b', '#21302b'],
 							borderWidth: 1
 						}
 					]
 				},
 				options: {
+					legend: {
+						labels: {
+							fontColor: '#a6bbb8',
+							fontSize: 15
+						}
+					},
 					scales: {
 						yAxes: [
 							{
 								ticks: {
+									fontColor: '#a6bbb8',
+									fontSize: 11,
 									beginAtZero: true
+								}
+							}
+						],
+						xAxes: [
+							{
+								ticks: {
+									fontColor: '#a6bbb8',
+									fontSize: 11
 								}
 							}
 						]
